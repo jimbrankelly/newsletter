8beeb024ea103f334b0023bb62e95e15c9a668b5
Jim Kelly <jimbrankelly91@gmail.com>
2026-03-14 20:19:35 -0500

    Updates for env and ssl mode

--- src/configuration.rs
+++ src/configuration.rs
@@ -23,22 +23,34 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode,};
+use sqlx::ConnectOptions;
 impl DatabaseSettings {
-    // Renamed from `connection_string_without_db`
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(&self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
     
     // Renamed from `connection_string`
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        //self.without_db().database(&self.database_name)
+        let options = self.without_db()
+            .database(&self.database_name)
+            .log_statements(tracing::log::LevelFilter::Trace);
+        options
     }
 }
 
