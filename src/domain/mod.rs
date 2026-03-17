76bb5364003fb1099ee5c88dce1f7ce4639492d1
Jim Kelly <jimbrankelly91@gmail.com>
2026-03-16 21:47:51 -0500

    Improving validation

---
+++ src/domain/mod.rs
@@ -0,0 +1,7 @@
+pub mod subscriber_email;
+pub mod subscriber_name;
+pub mod new_subscriber;
+
+pub use subscriber_name::SubscriberName;
+pub use new_subscriber::NewSubscriber;
+pub use subscriber_email::SubscriberEmail;
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
 //use tracing::Instrument;
+use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,7 +24,26 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+		// Return early if the name is invalid, with a 400
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+		// Return early if the name is invalid, with a 400
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let new_subscriber = NewSubscriber {
+        email: email,
+        name,
+    };
+    /*let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name).expect("Name validation failed."), //?,
+    };*/
+
+    match insert_subscriber(&pool, &new_subscriber).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish()
@@ -32,11 +52,12 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
 pub async fn insert_subscriber(
     pool: &PgPool,
-    form: &FormData,
+    new_subscriber: &NewSubscriber,
+    //form: &FormData,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
@@ -44,8 +65,8 @@ pub async fn insert_subscriber(
     VALUES ($1, $2, $3, $4)
             "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
