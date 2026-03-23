8e67cfe3dea8c4e7edc837723e777b1886820d22
Jim Kelly <jimbrankelly91@gmail.com>
2026-03-22 21:03:36 -0500

    email send queue ok

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use newsletter::{
     configuration::get_configuration,
+    issue_delivery_worker::run_worker_until_stopped,
     telemetry::{init_subscriber, get_subscriber,},
     startup::Application,
 };
+use std::fmt::{Display, Debug, };
+use tokio::task::JoinError;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
@@ -13,63 +16,47 @@ async fn main() -> anyhow::Result<()>{
     );
     init_subscriber(subscriber);
 
+
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
+    let application = Application::build(configuration.clone()).await?;
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
+
+    tokio::select! {
+        o = application_task => report_exit("API", o),
+        o = worker_task =>  report_exit("Background worker", o),
+    };
+
+
     Ok(())
 }
 
-/*#[tokio::main]
-async fn main_old() -> std::io::Result<()> {
+fn report_exit(
+    task_name: &str, 
+    outcome: Result<Result<(), impl Debug + Display>, JoinError>
+) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        }
+        Ok(Err(e)) => {
+            tracing::error!(
+                error.cause_chain = ?e, 
+                error.message = %e, 
+                "{} failed", 
+                task_name
+            )
+        }
 
-    let subscriber = get_subscriber(
-        "newsletter".into(), 
-        "info".into(),
-        std::io::stdout,
-    );
-    init_subscriber(subscriber);
-
-    // Panic if we can't read configuration
-    let configuration = get_configuration().expect("Failed to read configuration.");
-
-    /*let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(
-            &configuration.database.connection_string().expose_secret()
-        )
-        .expect("Failed to connect to Postgres.");*/
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        // `connect_lazy_with` instead of `connect_lazy`
-        .connect_lazy_with(configuration.database.with_db());
-
-    let _ = sqlx::migrate!("./migrations")
-        .run(&connection_pool)
-        .await
-        .unwrap_or_else(|e| {
-            tracing::warn!("Failed to migrate db: {:?}", e);
-            ()
-        } );
-
-    // Build an `EmailClient` using `configuration`
-    let sender_email = configuration.email_client.sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}", 
-        configuration.application.host,
-        configuration.application.port
-    );
-
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await
+        Err(e) => {
+            tracing::error!(
+                error.cause_chain = ?e, 
+                error.message = %e, 
+                "{}' task failed to complete", 
+                task_name
+            )
+        }
+    }
 }
-*/
+
